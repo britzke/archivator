7c2bbaff091a6c3094aff32078ac03fefa4b0f27
e0_hansen <e0_hansen@EDV007.eta.lette-verein.de>
2012-12-17 11:03:56 +0100

    #18 Test

---
+++ src/de/archivator/tests/LoginBeanTest.java
@@ -0,0 +1,70 @@
+package de.archivator.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Task #18
+ * @author e0_hansen
+ *
+ */
+public class LoginBeanTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.LoginBean#getKennwort()}.
+	 */
+	@Test
+	public void testGetKennwort() {
+		
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.LoginBean#setKennwort(java.lang.String)}.
+	 */
+	@Test
+	public void testSetKennwort() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.LoginBean#isAngemeldet()}.
+	 */
+	@Test
+	public void testIsAngemeldet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.LoginBean#setAngemeldet(boolean)}.
+	 */
+	@Test
+	public void testSetAngemeldet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.LoginBean#login()}.
+	 */
+	@Test
+	public void testLogin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.LoginBean#logout()}.
+	 */
+	@Test
+	public void testLogout() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
