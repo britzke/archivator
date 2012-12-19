4b137943e1080bb96a4a6709fa358d3c077cfd53
burghard.britzke <bubi@charmides.in-berlin.de>
2012-12-19 21:41:47 +0100

    #34 Lizenzvermerk in alle Java-Dateien eingefügt

--- src/de/archivator/beans/EntityManagerBean.java
+++ src/de/archivator/beans/EntityManagerBean.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of archivator, a software system for managing
+ * and retrieving archived items.
+ *
+ * Copyright (C) 2012  Burghard Britzke, bubi@charmides.in-berlin.de
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.archivator.beans;
 
 import java.io.Serializable;
@@ -27,11 +46,9 @@ public class EntityManagerBean implements Serializable {
 	 * Erzeugt eine neue EntityManagerBean. Initialisiert den EntityManager.
 	 */
 	public EntityManagerBean() {
-		System.out.println("entityManagerBean (Application) initializing...");
 		EntityManagerFactory emf = Persistence
-				.createEntityManagerFactory("mitglieder");
+				.createEntityManagerFactory("archivator");
 		entityManager = emf.createEntityManager();
-		System.out.println("entityManagerBean (Application) initialized");
 	}
 
 	/**
