59fbcb4a19e3805dd9e3e07afbd59afc251a02c6
burghard.britzke <bubi@charmides.in-berlin.de>
2013-01-01 19:50:59 +0100

    #23 Grundstruktur für den Test erstellt.

---
+++ src/de/archivator/tests/EditBeanTest.java
@@ -0,0 +1,117 @@
+/*
+ * This file is part of archivator, a software system for managing
+ * and retrieving archived items.
+ *
+ * Copyright (C) 2012  burghard.britzke
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.archivator.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.archivator.beans.EditBean;
+
+/**
+ * Testet die EditBean.
+ * @author bubi
+ */
+public class EditBeanTest {
+
+	private EditBean proband;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		proband = new EditBean();
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.EditBean#lösche()}.
+	 */
+	@Test
+	public void testLösche() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.EditBean#speichere()}.
+	 */
+	@Test
+	public void testSpeichere() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.EditBean#erstelle()}.
+	 */
+	@Test
+	public void testErstelle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.EditBean#loadNamen()}.
+	 */
+	@Test
+	public void testLoadNamen() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.EditBean#saveNamen()}.
+	 */
+	@Test
+	public void testSaveNamen() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.EditBean#loadOrganisationseinheiten()}.
+	 */
+	@Test
+	public void testLoadOrganisationseinheiten() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.EditBean#saveOrganisationseinheiten()}.
+	 */
+	@Test
+	public void testSaveOrganisationseinheiten() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.EditBean#loadSchlagworte()}.
+	 */
+	@Test
+	public void testLoadSchlagworte() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.archivator.beans.EditBean#saveSchlagworte()}.
+	 */
+	@Test
+	public void testSaveSchlagworte() {
+		fail("Not yet implemented");
+	}
+
+}
