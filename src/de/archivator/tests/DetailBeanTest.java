880ffc2dfd6daa6fc553d3fdcc87269d2da1428c
burghard.britzke <bubi@charmides.in-berlin.de>
2013-01-03 17:32:22 +0100

    #19 Tests für Getter und Setter entfernt

--- src/de/archivator/tests/DetailBeanTest.java
+++ src/de/archivator/tests/DetailBeanTest.java
@@ -37,21 +37,6 @@ public class DetailBeanTest {
 	public void setUp() throws Exception {
 	}
 
-	/**
-	 * Test method for {@link de.archivator.beans.DetailBean#getAktuellesArchivale()}.
-	 */
-	@Test
-	public void testGetAktuellesArchivale() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link de.archivator.beans.DetailBean#setAktuellesArchivale(de.archivator.entities.Archivale)}.
-	 */
-	@Test
-	public void testSetAktuellesArchivale() {
-		fail("Not yet implemented");
-	}
 
 	/**
 	 * Test method for {@link de.archivator.beans.DetailBean#back()}.
