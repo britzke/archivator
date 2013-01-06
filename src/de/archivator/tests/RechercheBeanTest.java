54f6460ffdc35b200821c32a2151a7014f338705
Atilla Schulz <atilla_schulz@yahoo.de>
2013-01-06 19:02:58 +0100

    Test entfernt

--- src/de/archivator/tests/RechercheBeanTest.java
+++ src/de/archivator/tests/RechercheBeanTest.java
@@ -75,7 +75,6 @@ public class RechercheBeanTest {
 			}
 		} catch (Exception e) {
 			fail("Dem suchKriterium wurde nicht \"[betreff] = \" hinzugefügt");
-			System.out.println("blubb");
 		}
 
 	}
