397a1f0bba11fc23f2d70b01e85139686e8ac687
burghard.britzke <bubi@charmides.in-berlin.de>
2013-01-06 23:34:46 +0100

    EntityManagerFactory ist ApplicationScoped anstatt EntityManager

---
+++ src/de/archivator/beans/EntityManagerFactoryBean.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of archivator, a software system for managing
+ * and retrieving archived items.
+ *
+ * Copyright (C) 2012  Burghard Britzke, bubi@charmides.in-berlin.de
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.archivator.beans;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Eine EntityManagerBean stellt der Applikation
+ * einen EntityManager zur Verfügung.
+ * @author bubi
+ */
+@ApplicationScoped
+public class EntityManagerFactoryBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Produces
+	private EntityManagerFactory entityManagerFactory;
+
+	/**
+	 * Erzeugt eine neue EntityManagerFactoryBean.
+	 */
+	public EntityManagerFactoryBean() {
+		entityManagerFactory = Persistence
+				.createEntityManagerFactory("archivator");
+	}
+
+	/**
+	 * Liefert die Eigenschaft entityManagerFactory.
+	 * @return the entityManager
+	 */
+	public EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
+}
