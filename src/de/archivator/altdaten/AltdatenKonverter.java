6886aacbdbffb9329d766a7252502e20b797a56b
jnaumann <payaso1900@gmx.de>
2013-01-13 18:40:10 +0100

    #7 ValidierungsHandler hinzugefügt

--- src/de/archivator/altdaten/AltdatenKonverter.java
+++ src/de/archivator/altdaten/AltdatenKonverter.java
@@ -10,7 +10,9 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import de.archivator.altdaten.model.ObjectFactory;
 import de.archivator.altdaten.model.TabelleX0020Archiv;
+import de.archivator.entities.Archivale;
 
 /**
  * @author e0_naumann
@@ -18,9 +20,10 @@ import de.archivator.altdaten.model.TabelleX0020Archiv;
  */
 public class AltdatenKonverter {
 	
-	
+	Archivale archiv;
+	TabelleX0020Archiv tabelle;
 	/**
-	 * 
+	 * Konverter der Daten aus einer xml Datei liest und diese in Java-objekte umwandelt
 	 */
 	public AltdatenKonverter() {
 		// TODO Auto-generated constructor stub
@@ -28,12 +31,27 @@ public class AltdatenKonverter {
 		try {
 			jaxbContext = JAXBContext.newInstance("de.archivator.altdaten.model");
 			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+			unmarshaller.setEventHandler(new AltdatenValidationEventHandler());
 			JAXBElement<TabelleX0020Archiv> bookingElement= (JAXBElement<TabelleX0020Archiv>)unmarshaller.unmarshal(new File("src/altdaten/Archivdatenbank.xml"));
-			TabelleX0020Archiv tabelle = bookingElement.getValue();
+			tabelle = bookingElement.getValue();
+		
 		} catch (JAXBException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+	/**
+	 * Methode, um die umgewandelten Daten in die neue Datenbank zuspeichern
+	 * 
+	 */
+	public void archivLaden(){
+		archiv = new Archivale();
+		
+		archiv.setSchubfach(tabelle.getSchubfachX0020Nummer());
+		archiv.setBetreff(tabelle.getBetreff());
+		archiv.setInhalt(tabelle.getInhalt());
+		
 		
 	}
 	
