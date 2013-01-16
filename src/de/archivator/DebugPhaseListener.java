e2dbac10afe4a8bcce072c1eca6c87713b6c1874
britzke <britzke@lette-verein.de>
2013-01-16 09:35:08 +0100

    Javadoc Dokumentation

--- src/de/archivator/DebugPhaseListener.java
+++ src/de/archivator/DebugPhaseListener.java
@@ -1,4 +1,22 @@
-
+/*
+ * This file is part of archivator, a software system for managing
+ * and retrieving archived items.
+ *
+ * Copyright (C) 2012  burghard.britzke
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.archivator;
 
 import java.util.List;
@@ -10,17 +28,22 @@ import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 
 /**
- * @author bubi
- *
+ * Ein DebugPhaseListener ist ein JSF PhaseListener,
+ * der den Beginn und das Ende jeder Phase des JSF-Lebenszyklus'
+ * auf der Konsole anzeigt.
+ * 
+ * @author burghard.britzke
  */
 public class DebugPhaseListener implements PhaseListener {
-
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	/**
+	 * Zeig das Ende einer Phase des JSF-Lebenszyklus'
+	 * auf der Konsole an. Zusätzlich werden alle Messages
+	 * angezeigt, die zum Ende der Phase in der JSF Message Queue
+	 * gespeichert sind.
+	 * 
 	 * @see javax.faces.event.PhaseListener#afterPhase(javax.faces.event.PhaseEvent)
 	 */
 	@Override
@@ -29,11 +52,13 @@ public class DebugPhaseListener implements PhaseListener {
 		FacesContext f= event.getFacesContext();
 		List<FacesMessage> ml = f.getMessageList();
 		for (FacesMessage m:ml){
-			System.out.println(m.getSummary()+": "+m.getDetail());
+			System.out.println("after Phase Message: "+m.getSummary()+": "+m.getDetail());
 		}
 	}
 
 	/**
+	 * Zeigt den Beginn einer Phase des JSF-Lebenszyklus'
+	 * auf der Konsole an.
 	 * @see javax.faces.event.PhaseListener#beforePhase(javax.faces.event.PhaseEvent)
 	 */
 	@Override
@@ -42,6 +67,8 @@ public class DebugPhaseListener implements PhaseListener {
 	}
 
 	/**
+	 * Zeigt an, dass der PhaseListener für jede Phase
+	 * registriert werden soll.
 	 * @see javax.faces.event.PhaseListener#getPhaseId()
 	 */
 	@Override
