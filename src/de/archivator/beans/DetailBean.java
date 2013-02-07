dcbb3189c6f90e12fee4358c01db0b0f9fea5327
burghard.britzke <bubi@charmides.in-berlin.de>
2013-02-07 15:31:29 +0100

    nicht benötigte Eigenschaften entfernt.

--- src/de/archivator/beans/DetailBean.java
+++ src/de/archivator/beans/DetailBean.java
@@ -24,10 +24,7 @@ import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 
 import org.primefaces.component.tabview.Tab;
 import org.primefaces.component.tabview.TabView;
@@ -53,9 +50,6 @@ import de.archivator.entities.Archivale;
 @SessionScoped
 public class DetailBean implements Serializable {
 	private static final long serialVersionUID = 1L;
-	@Inject
-	private transient EntityManagerFactory entityManagerFactory;
-	private EntityManager entityManager;
 
 	@Produces @AktuellesArchivale
 	private Archivale aktuellesArchivale;
