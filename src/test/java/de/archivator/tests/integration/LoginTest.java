4db1bda7fac2824cc9a656661d4115e05ff11065
burghard.britzke <bubi@charmides.in-berlin.de>
2014-01-10 14:42:02 +0100

    Lizenzheader eingefügt

--- src/test/java/de/archivator/tests/integration/LoginTest.java
+++ src/test/java/de/archivator/tests/integration/LoginTest.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of archivator, a software system for managing
+ * and retrieving archived items.
+ *
+ * Copyright (C) 2012  Janine Naumann
+ * 						Atilla Schulz
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.archivator.tests.integration;
 
 import static org.junit.Assert.*;
